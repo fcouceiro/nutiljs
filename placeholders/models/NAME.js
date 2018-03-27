6b2255805ef68c25e1c89ce7d850ac9fff1c0a8c
Francisco Couceiro <couceiro.f@gmail.com>
2018-03-27 01:03:21 +0100

    Adds placeholder for models

---
+++ placeholders/models/NAME.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+/// TODO: implement <Name> schema.
+/// Below is an example of using String, Number and sub-document properties.
+const <Name>Schema = new mongoose.Schema({
+  text: String,
+  number: Number,
+  subdocs: [{
+    text: String,
+    referenceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'ReferencedClassModelName'
+    }
+  }]
+});
+
+module.exports = mongoose.model('<Name>', <Name>Schema);
