ad4f4e8b13fd082a3c791371a1790b2cfd113384
Francisco Couceiro <couceiro.f@gmail.com>
2018-03-27 15:56:54 +0100

    Using child processes to generate files in parallel ⚡

--- src/computer.js
+++ src/computer.js
@@ -1,49 +1,38 @@
-const fs = require('fs')
 const path = require('path')
+const child_process = require('child_process');
 const inflection = require('inflection')
 const config = require('../gen-config.json')
-const generator = require('./generator')
 
 // Get Current Working Directory
 const sh = require('shelljs')
 const cwd = String(sh.pwd())
 
-// Promise handling
-const Q = require('q')
-
-// Denodeify (turn into promise)
-const readdir = Q.denodeify(fs.readdir)
-
-// Retrieves placeholders file names
-function getPlaceholdersFileNames(placeholdersPath) {
-  return readdir(placeholdersPath, 'utf8')
-    .then(files => files.filter((placeholderFile) => placeholderFile.includes(".js")))
-}
-
 // Distribute work
 function run(args) {
   // Generate services
   if (args.service) {
-    let servicePlaceholders = getPlaceholdersFileNames(path.join(__dirname, config.servicesPlaceholdersPath))
-      .then(servicesPlaceholdersFiles => generator.start(
-        args.service.toLowerCase(),
-        path.join(__dirname, config.servicesPlaceholdersPath),
-        servicesPlaceholdersFiles,
-        path.join(cwd, config.servicesPath, inflection.pluralize(args.service).toLocaleLowerCase())
-      ))
-      .then(() => console.log("🔰\tGenerated", args.service.toLowerCase(), "service..."))
+    let processArgs = {
+      argument : args.service,
+      type : 'service',
+      placeholdersPath : path.join(__dirname, config.servicesPlaceholdersPath),
+      outputDir : path.join(cwd, config.servicesPath, inflection.pluralize(args.service).toLowerCase())
+    }
+
+    // Spawn process
+    child_process.fork('./src/process', [JSON.stringify(processArgs)]);    
   }
 
   // Generate controllers
   if (args.controller) {
-    let controllerPlaceholders = getPlaceholdersFileNames(path.join(__dirname, config.controllersPlaceholdersPath))
-      .then(controllersPlaceholdersFiles => generator.start(
-        args.controller.toLowerCase(),
-        path.join(__dirname, config.controllersPlaceholdersPath),
-        controllersPlaceholdersFiles,
-        path.join(cwd, config.controllersPath)
-      ))
-      .then(() => console.log("🔰\tGenerated", args.controller, "controller..."))
+    let processArgs = {
+      argument : args.controller,
+      type : 'controller',
+      placeholdersPath : path.join(__dirname, config.controllersPlaceholdersPath),
+      outputDir : path.join(cwd, config.controllersPath)
+    }
+
+    // Spawn process
+    child_process.fork('./src/process', [JSON.stringify(processArgs)]);  
   }
 }
 
