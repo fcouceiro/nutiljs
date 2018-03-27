5ae7db0230afd19080ebea74d9e9fb15078a098c
Francisco Couceiro <couceiro.f@gmail.com>
2018-03-27 02:56:53 +0100

    Refactors generator logic

--- src/generator.js
+++ src/generator.js
@@ -1,169 +1,38 @@
-const sh = require('shelljs');
+const replace = require('./replace')
+const utils = require('./utils')
 const fs = require('fs')
 const path = require('path')
-
-// Handle pluralization/singularization transforms
-const inflection = require('inflection')
-
-// Promise handling
+const pipeStreams = require('pipe-streams-to-promise')
 const Q = require('q')
 
-// Denodeify (turn into promises)
-const readdir = Q.denodeify(fs.readdir)
-const readFile = Q.denodeify(fs.readFile)
+module.exports.start = (arg, placeholderDir, placeholderFiles, outputDir) => {
+  // Make output dir
+  utils.mkdir(outputDir)
 
-// Stream handling
-const stream = require('stream')
-const Readable = stream.Readable
-const replaceStream = require('replacestream')
-const sink = require('stream-sink')
-const pipeStreams = require('pipe-streams-to-promise')
+  let copyOperations = []
 
-// Get Current Working Directory
-const cwd = String(sh.pwd())
+  // Handle each placeholder
+  for (var i = 0, len = placeholderFiles.length; i < len; ++i){
+    // Generate output file name
+    let placeholderFileName = placeholderFiles[i]
+    let fileNameStream = replace.allPlaceholderOccurrences(placeholderFileName, arg)
+    
+    let copyOperation = utils.sinkStream(fileNameStream)
+      .then(outputFileName => {
+        // Generate boilerplate from placeholder file
+        let copyStream = replace.allPlaceholderOccurrences(fs.createReadStream(path.join(placeholderDir, placeholderFileName)), arg)
+        
+        // Copy to destination
+        let writeStream = fs.createWriteStream(path.join(outputDir, outputFileName))
 
-// Placeholder keys (lazy loaded)
-var PLACEHOLDER_KEYS;
+        return pipeStreams([copyStream, writeStream])
+      })
+      .catch(error => console.error(error))
 
-// Entry-point
-module.exports.run = function (cliArgs) {
-  return parseConfig()
-    .then(config => {
-      // Cache placeholder keys
-      PLACEHOLDER_KEYS = config.placeholderKeys
-      let modelStream = replacePlaceholderOccurrences(fs.createReadStream(path.join(__dirname, "../placeholders","models","NAME.js")), cliArgs.controller)
-      console.log(cwd)
-      modelStream.pipe(fs.createWriteStream(path.join(__dirname, "../test.js")))
-
-      // sinkStream(modelStream)
-      //   .then((data) => {
-      //     console.log(Buffer.concat(data).toString('utf8'))
-      //   })
-      //   .catch((error) => {
-      //     console.error(data)
-      //   })
-
-      // Parse services placeholders
-      return parsePlaceholders(path.join(__dirname, config.servicesPlaceholdersPath))
-        .then(servicesPlaceholdersFiles => {
-          generate(cliArgs, config, servicesPlaceholdersFiles)
-        })
-    })
-}
-
-// Parse placeholders
-function parsePlaceholders(placeholdersPath) {
-  return readdir(placeholdersPath, 'utf8')
-    .then(files => {
-      return files.filter((placeholderFile) => placeholderFile.includes(".js"))
-    })
-}
-
-// Parse configuration
-function parseConfig() {
-  return readFile(path.join(__dirname, '..', 'gen-config.json'), 'utf8')
-    .then(data => {
-      return JSON.parse(data)
-    });
-}
-
-// Generate files
-function generate(argv, config, servicesPlaceholderFiles) {
-  let serviceArg = argv.s || argv.service || null
-  if (serviceArg) {
-    // Transform service name
-    serviceArg = inflection.pluralize(serviceArg).toLowerCase()
-    console.log("🔰\tGenerating", serviceArg, "service...")
-
-    let serviceDir = path.join(cwd, config.servicesPath, serviceArg)
-    let serviceNamePlural = inflection.camelize(serviceArg)
-    let serviceNameSingular = inflection.singularize(serviceNamePlural)
-
-    // make services dir
-    mkdir(path.join(cwd, config.servicesPath))
-
-    // make service dir
-    mkdir(serviceDir)
-
-    let copies = []
-    servicesPlaceholderFiles.forEach(servicePlaceholderFile => {
-      // Generate service name
-      let serviceName;
-      if (servicePlaceholderFile.includes("NAMES")) {
-        serviceName = servicePlaceholderFile.replace("NAMES", serviceNamePlural)
-      }
-      else if (servicePlaceholderFile.includes("NAME")) {
-        serviceName = servicePlaceholderFile.replace("NAME", serviceNameSingular)
-      }
-      else {
-        throw new Error("Failed to generate", servicePlaceholderFile)
-        return
-      }
-
-      // Copy file
-      let servicePath = path.join(serviceDir, serviceName)
-      let servicePlaceholderPath = path.join(__dirname, config.servicesPlaceholdersPath, servicePlaceholderFile)
-      copies.push(copyFile(servicePlaceholderPath, servicePath, serviceNameSingular))
-    });
-
-    Q.all(copies).then(() => console.log("\t", serviceArg, "service completed ✅"))
+      // Push operation
+      copyOperations.push(copyOperation)
   }
 
-  let controllerArg = argv.c || argv.controller || null
-  if (controllerArg) {
-    // Transform controller name
-    controllerArg = inflection.singularize(controllerArg).toLowerCase()
-    console.log("🔰\tGenerating", controllerArg, "controller...")
-
-    // make controllers dir
-    mkdir(path.join(cwd, config.controllersPath))
-
-    // Generate controller name
-    let controllerPlaceholderName = path.parse(path.join(__dirname, config.controllerPlaceholderFile)).base
-    let controllerName = controllerPlaceholderName.replace("NAME", controllerArg)
-
-    // Copy file
-    let controllerPath = path.join(cwd, config.controllersPath, controllerName)
-    copyFile(path.join(__dirname, config.controllerPlaceholderFile), controllerPath, controllerArg)
-      .then(() => console.log("\t", controllerArg, "controller completed ✅"))
-  }
-}
-
-// Utils
-function mkdir(dir) {
-  if (!fs.existsSync(dir))
-    fs.mkdirSync(dir)
-}
-
-function copyFile(source, target, replaceContent, logEnabled) {
-  let streams = []
-
-  // Source read stream
-  streams.push(fs.createReadStream(source))
-
-  // Replace text occurrences
-  if (replaceContent) {
-    streams.push(replaceStream("<name>", replaceContent.toLowerCase(), { ignoreCase: false }))
-    streams.push(replaceStream("<NAME>", replaceContent.toUpperCase(), { ignoreCase: false }))
-  }
-
-  // Target write stream
-  streams.push(fs.createWriteStream(target))
-
-  // Pipe source to target
-  let promise = pipeStreams(streams)
-  if (logEnabled)
-    promise.then(logCompletion)
-
-  return promise
-}
-
-function sinkStream(stream){
-  // Sink stream result as object and catch errors
-  return strem.pipe(sink.object({ upstreamError: true }))
-}
-
-function logCompletion(writeStream) {
-  if (writeStream)
-    console.log(writeStream.path, "✅")
+  // Await all
+  return Q.all(copyOperations)
 }
--- src/replace.js
+++ src/replace.js
@@ -1,13 +1,10 @@
-// Description:
-//
-// Replaces placeholder occurrences in every format/case,
-// from either a string or a stream.
-//
-
+const inflection = require('inflection')
 const stream = require('stream')
 const Readable = stream.Readable
 const replaceStream = require('replacestream')
+const PLACEHOLDER_KEYS = require('../gen-config.json').placeholderKeys
 
+// Creates stream from string content
 function streamFromString(string) {
   var s = new Readable
   s._read = function noop() { };
@@ -17,39 +14,46 @@ function streamFromString(string) {
   return s
 }
 
-module.exports = {
-  replacePlaceholderOccurrences: function (search, replace) {
-    // Create stream from search if it isnt already
-    if (!(search instanceof stream.Readable)) {
-      search = streamFromString(search)
-    }
-
-    // Generate replace values in every format
-    let replaceSingular = inflection.singularize(replace)
-    let replacePlural = inflection.pluralize(replace)
-
-    let values = {
-      singular: {
-        lowerCase: replaceSingular.toLowerCase(),
-        upperCase: replaceSingular.toUpperCase(),
-        pascalCase: inflection.camelize(replaceSingular)
-      },
-      plural: {
-        lowerCase: replacePlural.toLowerCase(),
-        upperCase: replacePlural.toUpperCase(),
-        pascalCase: inflection.camelize(replacePlural)
-      }
-    }
-
-    // Transform input
-    return search
-      // Singular
-      .pipe(replaceStream(PLACEHOLDER_KEYS.singular.lowerCase, values.singular.lowerCase, { ignoreCase: false }))
-      .pipe(replaceStream(PLACEHOLDER_KEYS.singular.upperCase, values.singular.upperCase, { ignoreCase: false }))
-      .pipe(replaceStream(PLACEHOLDER_KEYS.singular.pascalCase, values.singular.pascalCase, { ignoreCase: false }))
-      // Plural
-      .pipe(replaceStream(PLACEHOLDER_KEYS.plural.lowerCase, values.plural.lowerCase, { ignoreCase: false }))
-      .pipe(replaceStream(PLACEHOLDER_KEYS.plural.upperCase, values.plural.upperCase, { ignoreCase: false }))
-      .pipe(replaceStream(PLACEHOLDER_KEYS.plural.pascalCase, values.plural.pascalCase, { ignoreCase: false }))
+// Replaces placeholder occurrences in every format/case,
+// from a search string or stream.
+//
+// Returns the transformed stream.
+function replace(search, replace) {
+  // Create stream from search if it isnt already
+  if (!(search instanceof stream.Readable)) {
+    search = streamFromString(search)
   }
+
+  // Generate replace values in every format
+  let replaceSingular = inflection.singularize(replace)
+  let replacePlural = inflection.pluralize(replace)
+
+  let values = {
+    singular: {
+      lowerCase: replaceSingular.toLowerCase(),
+      upperCase: replaceSingular.toUpperCase(),
+      pascalCase: inflection.camelize(replaceSingular)
+    },
+    plural: {
+      lowerCase: replacePlural.toLowerCase(),
+      upperCase: replacePlural.toUpperCase(),
+      pascalCase: inflection.camelize(replacePlural)
+    }
+  }
+
+  // Transform input
+  return search
+    // Singular
+    .pipe(replaceStream(PLACEHOLDER_KEYS.singular.lowerCase, values.singular.lowerCase, { ignoreCase: false }))
+    .pipe(replaceStream(PLACEHOLDER_KEYS.singular.upperCase, values.singular.upperCase, { ignoreCase: false }))
+    .pipe(replaceStream(PLACEHOLDER_KEYS.singular.pascalCase, values.singular.pascalCase, { ignoreCase: false }))
+    // Plural
+    .pipe(replaceStream(PLACEHOLDER_KEYS.plural.lowerCase, values.plural.lowerCase, { ignoreCase: false }))
+    .pipe(replaceStream(PLACEHOLDER_KEYS.plural.upperCase, values.plural.upperCase, { ignoreCase: false }))
+    .pipe(replaceStream(PLACEHOLDER_KEYS.plural.pascalCase, values.plural.pascalCase, { ignoreCase: false }))
+}
+
+// Expose API
+module.exports = {
+  allPlaceholderOccurrences: replace
 }
