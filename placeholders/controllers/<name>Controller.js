3e0b898ef33afa14052ad2723fca0f7c3406d599
Francisco Couceiro <couceiro.f@gmail.com>
2018-03-27 18:23:52 +0100

    Implements default methods

--- placeholders/controllers/<name>Controller.js
+++ placeholders/controllers/<name>Controller.js
@@ -1,4 +1,38 @@
-module.exports = function(){
+module.exports = function () {
   // TODO: implement <name> controller
   throw new Error("<name>Controller not yet implemented.")
+}
+
+function getAll(req, res) {
+  // TODO: implement <name> controller - index
+  throw new Error("<name>Controller::getAll not yet implemented.")
+}
+
+function create(req, res) {
+  // TODO: implement <name> controller - create
+  throw new Error("<name>Controller::create not yet implemented.")
+}
+
+function get(req, res) {
+  // TODO: implement <name> controller - get
+  throw new Error("<name>Controller::get not yet implemented.")
+}
+
+function update(req, res) {
+  // TODO: implement <name> controller - update
+  throw new Error("<name>Controller::update not yet implemented.")
+}
+
+function remove(req, res) {
+  // TODO: implement <name> controller - remove
+  throw new Error("<name>Controller::remove not yet implemented.")
+}
+
+// Expose API
+module.exports = {
+  getAll: getAll,
+  create: create,
+  get: get,
+  update: update,
+  remove: remove
 }
