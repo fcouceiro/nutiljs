c1bc0d7a630a7e8f8be2b1c3b56a9d384e34ac0e
Francisco Couceiro <couceiro.f@gmail.com>
2018-03-31 02:02:36 +0100

    🆕  Adds update basic placeholder

--- placeholders/services/update[Name].js
+++ placeholders/services/update[Name].js
@@ -1,3 +1,7 @@
-module.exports = function(){
-  // update
+const [Name] = require('../../models/[name]')
+const findById = require('./find[Name]ById')
+
+module.exports = function (id, props) {
+  return [Name].update({ _id: id }, props)
+    .then(() => findById(id))
 }
